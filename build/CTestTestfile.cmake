678b577097cade374ba52f5399d3d1bbbf8d6038
sopper08 <sopper08@gmail.com>
2019-03-15 15:02:42 +0800

    urdf without inertial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/sopper08/TMP/mbot/src
+# Build directory: /home/sopper08/TMP/mbot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(model)
